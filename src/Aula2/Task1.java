17522d35c9bfaa161ed88d8e7749fa2faa4471cf
ALUNO VR SOFTWARE <avrsoftware@SITREDES.LOCAL>
2026-05-02 11:56:03 -0300

    Adição da segunda atividade

--- src/Aula2/Task1.java
+++ src/Aula2/Task1.java
@@ -1,7 +1,7 @@
 public class Task1{
     public static void main(String[] args){
         for (int i = 1; i <= 12; i++){
-            System.out.println("Relatório do mês " + i + " gerado!");
+            System.out.println("Relatório do mês " + i);
         }
         System.out.println("\nRelatórios concluídos!");
     }
---
+++ src/Aula2/Task2.java
@@ -0,0 +1,42 @@
+/*
+Programa de cadastro de usuário, onde o usuário deve inserir seu nome, idade e email. 
+O programa deve validar os dados inseridos e exibir mensagens de erro caso os dados sejam inválidos. 
+Caso os dados sejam válidos, o programa deve exibir uma mensagem de sucesso.
+*/ 
+
+import java.util.Scanner;
+
+public class Task2{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite o seu email: ");
+        scanner.nextLine();
+        String email = scanner.nextLine();
+        
+        if (nome.equals("")){
+            System.out.println("\nNome inválido, tente novamente.\n");
+        } 
+        else if (idade <= 0){
+            System.out.println("\nIdade inválida, tente novamente.\n");
+        } 
+        else if (!email.contains("@")){
+            System.out.println("\nEmail inválido, tente novamente.\n");
+        } 
+        else {
+            System.out.println("\nUsuário cadastrado com sucesso!\n");
+        }
+        scanner.close();
+    }
+}
+
+/* 
+System.out.println(nome.equals("")); Verifica se é igual a uma condição
+System.out.println(nome.contains("")); Verifica se contem algo em uma string
+*/ 
