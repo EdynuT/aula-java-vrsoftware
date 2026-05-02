6aa8cd10244b2396d33984928fddaca6a16615af
ALUNO VR SOFTWARE <avrsoftware@SITREDES.LOCAL>
2026-05-02 09:34:29 -0300

    Correção do exercício

--- src/Aula2/Task1.java
+++ src/Aula2/Task1.java
@@ -1,16 +1,8 @@
-import java.util.Scanner;
-
 public class Task1{
     public static void main(String[] args){
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite um número inteiro: ");
-        int numero = scanner.nextInt();
-
-        for (int i = 1; i <= numero; i++){
-            System.out.println("Contagem: " + i);
+        for (int i = 1; i <= 12; i++){
+            System.out.println("Relatório do mês " + i + " gerado!");
         }
-        scanner.close();
-        System.out.println("Finalizado!");
+        System.out.println("\nRelatórios concluídos!");
     }
 }
