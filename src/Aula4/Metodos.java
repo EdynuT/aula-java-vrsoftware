2a8ba4801fdf3e1b2429ea0d594f5456e6b44cfe
ALUNO VR SOFTWARE <avrsoftware@SITREDES.LOCAL>
2026-05-23 12:00:39 -0300

    Introdução a metodos em Java

--- src/Aula4/Metodos.java
+++ src/Aula4/Metodos.java
@@ -4,29 +4,61 @@ public class Metodos {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //        double media = calcularMedia(2d,3d);
-        
+        //
         //        imprimirFrase();
         //        System.out.println(soma);
         //        System.out.println(media);
-        int soma1 = somar(2, 3);
-        int soma2 = somar(3.6, 4.4);
-        int soma3 = somar(1, 2, 3);
+        //int soma1 = somar(2, 3);
+        //int soma2 = somar(3.6, 4.4);
+        //int soma3 = somar(1, 2, 3);
+        //
+        //double resultado = 0d;
+        //
+        //if (soma1 > 3) {
+        //    double numero1 = calcularMedia(soma1, 10);
+        //    resultado = numero1;
+        //}
+        //System.out.println(resultado);
 
-        double resultado = 0d;
-
-        if (soma1 > 3) {
-            double numero1 = calcularMedia(soma1, 10);
-            resultado = numero1;
-        }
-        System.out.println(resultado);
+        String nome = lerNome();
+        double media = calcularMedia();
+        exibirResultado(nome, media);
     }
+        // Leitura
+        private static String lerNome() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Digite o nome do aluno: ");
+            return scanner.nextLine();
+        }
+
+        // Calcular
+        private static double calcularMedia() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Digite a primeira nota: ");
+            double nota1 = scanner.nextDouble();
+            System.out.print("Digite a segunda nota: ");
+            double nota2 = scanner.nextDouble();
+            System.out.print("Digite a terceira nota: ");
+            double nota3 = scanner.nextDouble();
+            
+            return (nota1 + nota2 + nota3) / 3;
+        }
+        
+        // Exibir
+        private static void exibirResultado(String nome, double media) {
+            System.out.print(nome + ": " + media);
+            
+            if (media >= 7) {
+                System.out.println(" Aprovado!");
+            } 
+            else {
+                System.out.println(" Reprovado!");
+            }
+        }
 
     // Preferi deixar os métodos como private, 
     // ja que n faz sentido chamar esses métodos de fora da classe
-    private static double calcularMedia(double a, double b) {
-        return (a + b) / 2;
-    }
-
+    
     // Overload de metodos de soma
     private static int somar(int a, int b) {
         return a + b;
@@ -36,9 +68,5 @@ public class Metodos {
     }
     private static int somar(int a, int b, int c) {
         return a + b + c;
-    }
-
-    private static void imprimirFrase() {
-        System.out.println("Qualquer frase");
     }
 }
